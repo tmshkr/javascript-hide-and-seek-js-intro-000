bfbd4ddee018da96c0510d84e7fc6b828b484b66
Tim Shaker <timshaker@gmail.com>
2018-08-22 04:02:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  let nodes = document.querySelectorAll('.ranked-list li')
+  
+  for (let i = 0; i < nodes.length; i++)
+    nodes[i].innerHTML = parseInt(nodes[i].innerHTML) + n
+}
+
+
+function deepestChild(){
+  let current = document.getElementById('grand-node')
+
+  while (current.childElementCount > 0)
+    current = current.children[0]
+
+  return current
+}
