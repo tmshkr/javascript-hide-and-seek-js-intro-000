6d4977e70104fa3031a789d61fe44dc8f8fbd08d
Tim Shaker <timshaker@gmail.com>
2018-08-22 03:57:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  let nodes = document.querySelectorAll('.ranked-list li')
+  
+  for (let i = 0; i < nodes.length; i++)
+    nodes[i].innerText = parseInt(nodes[i].innerText) + n
+}
+
+function deepestChild(){
+  
+}
